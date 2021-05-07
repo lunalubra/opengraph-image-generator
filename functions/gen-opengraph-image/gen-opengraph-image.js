25e8f5cadaf2560986cf15a27fcc90fea41a4ccc
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-07 16:50:52 +0200

    Added playwight!

---
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -0,0 +1,35 @@
+const playwright = require('playwright-aws-lambda');
+
+exports.handler = async function (event, ctx) {
+  const browser = await playwright.launchChromium();
+  const context = await browser._defaultContext;
+  const page = await context.newPage();
+  await page.setContent(`<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+    </head>
+  
+    <body>
+      <div id="corgi"><div>CORGIIIS</div></div>
+    </body>
+  </html>
+  `);
+  const boundingRect = await page.evaluate(() => {
+    const corgi = document.getElementById('corgi');
+    const { x, y, width, height } = corgi.children[0].getBoundingClientRect();
+    return { x, y, width, height };
+  });
+
+  const screenshotBuffer = await page.screenshot({ clip: boundingRect });
+  await browser.close();
+  return {
+    isBase64Encoded: true,
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'image/png',
+      'Content-Length': screenshotBuffer.length.toString()
+    },
+    body: screenshotBuffer.toString('base64')
+  };
+};
